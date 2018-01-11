6a6862e191426120a25108589b06194a03f71d78
DanAparicio22 <daparicio52@gmail.com>
2018-01-11 15:20:15 -0400

    TG-5 (Create ToDo) Add | Add buttons and list of all activities

--- ToDoListApplication/src/app/activity-list/activity-list.component.ts
+++ ToDoListApplication/src/app/activity-list/activity-list.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Activity} from '../shared/activity.model';
+import {ActivityService} from '../shared/activity.service';
+import { element } from 'protractor';
 @Component({
   selector: 'app-activity-list',
   templateUrl: './activity-list.component.html',
-  styleUrls: ['./activity-list.component.css']
+  styleUrls: ['./activity-list.component.css'],
+  providers: [ActivityService]
 })
 export class ActivityListComponent implements OnInit {
-  constructor() { }
-
+  activityList: Activity[];
+  constructor(private activityService: ActivityService) { }
   ngOnInit() {
+    const x = this.activityService.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.activityList = [];
+      // tslint:disable-next-line:no-shadowed-variable
+      item.forEach( element => {
+        const y = element.payload.toJSON();
+        y['$key'] = element.key;
+        this.activityList.push(y as Activity);
+      });
+    });
   }
-
 }
